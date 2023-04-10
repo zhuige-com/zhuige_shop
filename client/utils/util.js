984718ce16d41de02e98f89c2a675cc0c4608e39
longwenjunjie <longwenjunj@163.com>
2023-04-10 12:04:13 +0800

    ## 1.3.0（2023-04-10） 1. 修复跳转小程序提示的问题

--- client/utils/util.js
+++ client/utils/util.js
@@ -146,9 +146,12 @@ function openLink(link) {
 			let params = {
 				appId: appid,
 				fail: res => {
-					uni.setClipboardData({
-						data: link
-					});
+					// uni.setClipboardData({
+					// 	data: link
+					// });
+					if (res.errMsg && res.errMsg.indexOf('cancel') < 0) {
+						Alert.toast(res.errMsg)
+					}
 				}
 			};
 			if (page != '') {
@@ -170,9 +173,12 @@ function openLink(link) {
 			let params = {
 				finderUserName: finder,
 				fail: res => {
-					uni.setClipboardData({
-						data: link
-					});
+					// uni.setClipboardData({
+					// 	data: link
+					// });
+					if (res.errMsg && res.errMsg.indexOf('cancel') < 0) {
+						Alert.toast(res.errMsg)
+					}
 				}
 			};
 
