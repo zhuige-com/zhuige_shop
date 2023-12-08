16895359af5232bedba235323c09d2424e6dfed2
longwenjunjie <longwenjunj@163.com>
2023-12-08 09:51:31 +0800

    ## 1.4.9（2023-12-08） 1. 新增更新功能

--- client/App.vue
+++ client/App.vue
@@ -1,6 +1,6 @@
 <script>
 	/*
-	 * 追格商城小程序 v1.4.8
+	 * 追格商城小程序 v1.4.9
 	 * 作者: 追格
 	 * 文档: https://www.zhuige.com/docs/sc.html
 	 * gitee: https://gitee.com/zhuige_com/zhuige_shop
@@ -23,6 +23,27 @@
 				cart = [];
 			}
 			store.commit('cartSet', cart);
+			
+			const updateManager = wx.getUpdateManager()
+			updateManager.onCheckForUpdate(function(res) {
+				// 请求完新版本信息的回调
+				// console.log(res.hasUpdate)
+			})
+			updateManager.onUpdateReady(function() {
+				wx.showModal({
+					title: '更新提示',
+					content: '新版本已经准备好，是否重启应用？',
+					success: function(res) {
+						if (res.confirm) {
+							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+							updateManager.applyUpdate()
+						}
+					}
+				})
+			})
+			updateManager.onUpdateFailed(function() {
+				// 新版本下载失败
+			})
 		},
 
 		onShow() {
